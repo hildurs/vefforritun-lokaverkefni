6b22f1b2b163891fdb6ab9288ad26f5b65010998
gunnijons1992 <gunnijons1992@gmail.com>
2016-11-19 18:23:50 +0000

    bull

--- src/public/javascripts/games.js
+++ src/public/javascripts/games.js
@@ -1,16 +1,15 @@
 'use strict';
-var init = function init() {
-  getData();
-};
+let init = () =>  {
+   getData();
+ }
 
-localStorage.setItem("lang", "is");
+ let getData = () => {
+   $("#games").html(' ');
+   $.get(`http://apis.is/sports/football`, (data) => {
+     data.map((football) => {
+       $('#games').append("<tr><td>" + football.date + "</td><td>" + football.time + "</td><td>" + football.tournament + "</td><td>" + football.location + "</td><td>" + football.homeTeam + "</td><td>" + football.awayTeam + "</td><td>" + football.undefined + "</td></tr>");
+     });
+   });
+ }
 
-var getData = function getData()  {
-  ("#games").html(' ');
-  $.get("http://apis.is/sports/football", function (data) {
-    data.map(function (football) {
-      $('#games').append("<tr><td>" + football.date + "</td><td>" + football.time + "</td><td>" + football.tournament + "</td><td>" + football.location + "</td><td>" + football.homeTeam + "</td><td>" + football.awayTeam + "</td><td>" + football.undefined + "</td></tr>");
-    )}
-  })};
-
-init();
+ init();
