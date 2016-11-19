c19db6aa5e87601e6f3b9f0fc23192dc116fc6d1
hildurs <hos18@hi.is>
2016-11-19 21:11:55 +0000

    lint

---
+++ dist/public/javascripts/games.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let init = () => {
+  getData();
+};
+
+let getData = () => {
+  $("#games").html(' ');
+  $.get(`http://apis.is/sports/football`, data => {
+    data.map(football => {
+      $('#games').append("<tr><td>" + football.date + "</td><td>" + football.time + "</td><td>" + football.tournament + "</td><td>" + football.location + "</td><td>" + football.homeTeam + "</td><td>" + football.awayTeam + "</td><td>" + football.undefined + "</td></tr>");
+    });
+  });
+};
+
+init();
