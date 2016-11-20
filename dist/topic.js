7ef838abef6726c1b3373f12c1ba2acbb4be7c4f
hildurs <hos18@hi.is>
2016-11-20 20:55:52 +0000

    confirm message

--- dist/topic.js
+++ dist/topic.js
@@ -1,12 +1,8 @@
-function myFunction() {
+function confirmTopic() {
     var x;
     if (confirm("Ertu viss um að þú viljir vista umræðuþráðinn?") == true) {
-        setTimeout(function () {
-            alert("moo");
-        }, 10000);
         x = "Umræðuþráður hefur verið vistaður!";
     } else {
         x = "Umræðuþráður var ekki vistaður!";
     }
-    document.getElementById("ave-topi").innerHTML = x;
 }
